91eafa1b47994fab240e38be5478d19a36aed514
wjr <1702526238@qq.com>
2025-03-09 17:03:25 +0800

    2025_3_9

--- helloworld/src/helloworld.cpp
+++ helloworld/src/helloworld.cpp
@@ -2,8 +2,8 @@
 // Created by wjr on 25-2-26.
 //
 #include "ros/ros.h"
-int main(int argc,char *argv[]) {
-    setlocale(LC_ALL,"");
+int main(int argc,char *argv[]) {//argc封装参数个数 argv是封装参数的数组
+    setlocale(LC_ALL,"");//定义语言
     ros::init(argc,argv,"helloworld_node");
     ROS_INFO("helloworld!哈哈");
     return 0;
--- plumbing_sub/cmake-build-debug/catkin_generated/setup_cached.sh
+++ plumbing_sub/cmake-build-debug/catkin_generated/setup_cached.sh
@@ -10,5 +10,6 @@
 export CMAKE_PREFIX_PATH="/home/wjr/ros_ws/src/plumbing_sub/cmake-build-debug/devel:$CMAKE_PREFIX_PATH"
 export LD_LIBRARY_PATH="/home/wjr/ros_ws/src/plumbing_sub/cmake-build-debug/devel/lib:$LD_LIBRARY_PATH"
 export PKG_CONFIG_PATH="/home/wjr/ros_ws/src/plumbing_sub/cmake-build-debug/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PYTHONPATH="/home/wjr/ros_ws/src/plumbing_sub/cmake-build-debug/devel/lib/python3/dist-packages:$PYTHONPATH"
 export ROSLISP_PACKAGE_DIRECTORIES="/home/wjr/ros_ws/src/plumbing_sub/cmake-build-debug/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
 export ROS_PACKAGE_PATH="/home/wjr/ros_ws/src/plumbing_sub:$ROS_PACKAGE_PATH"
